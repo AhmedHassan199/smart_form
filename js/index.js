404daf91326678293b98743061ac51dfcf4bb334
Ahmed Mohamed <amohamed@uittai.com>
2024-06-08 15:10:26 +0300

    updates

--- js/index.js
+++ js/index.js
@@ -3,10 +3,12 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     var userEmail = emailElement.value;
     var userPassword = passwordElement.value;
     var foundUser = findUserByEmailAndName(userEmail, userName = null ,userPassword , "login");
-authUserName =foundUser.user_name;
-    localStorage.setItem('auth_user_email', authUserName);
     if (foundUser) {
-       
+        authUserName =foundUser.user_name;
+        localStorage.setItem('auth_user_email', authUserName);
         window.location.href = './home.html';
+    }else{
+    Swal.fire("please check password or email to login");
+
     }
 }});
